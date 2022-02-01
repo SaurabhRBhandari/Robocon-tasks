b55cda2ebc54569e16a01925abbab5faf3d403ab
SaurabhRBhandari <bhandarisourabhrahul@gmail.com>
2022-02-01 11:51:34 +0530

    task 2 part 2 done

---
+++ Image_Processing/Task_2_Arrows/advance.py
@@ -0,0 +1,45 @@
+# importing necessary libraries
+import cv2
+import numpy as np
+
+image = cv2.imread('Arrow_3.jpg', cv2.THRESH_BINARY)
+image = cv2.blur(image, (6, 6))
+corners = cv2.goodFeaturesToTrack(image, 27, 0.01, 10)
+corners = np.int0(corners).squeeze()
+center = np.int0(np.mean(corners, axis=0))
+tip = np.zeros_like(center)
+is_rect = False
+
+
+def angle(a, b, c):
+    points = np.array([a, b, c])
+    A = points[1] - points[0]
+    B = points[2] - points[1]
+    C = points[0] - points[2]
+
+    angles = []
+    for e1, e2 in ((A, -B), (B, -C), (C, -A)):
+        num = np.dot(e1, e2)
+        denom = np.linalg.norm(e1) * np.linalg.norm(e2)
+        angles.append(np.arccos(num/denom) * 180 / np.pi)
+
+    return(any(angle > 75 and angle < 105 for angle in angles))
+
+
+for c in corners:
+    other_vertices = []
+    for o in corners:
+        if np.array_equal(o, c):
+            continue
+        else:
+            other_vertices.append(o)
+    p1, p2, p3, p4 = other_vertices
+    if(angle(p1, p2, p3) and angle(p2, p3, p4) and angle(p3, p4, p1) and angle(p4, p1, p2)):
+        tip = c
+        break
+print(tip)
+print(center)
+
+
+    
+
--- Image_Processing/Task_2_Arrows/basic.py
+++ Image_Processing/Task_2_Arrows/basic.py
@@ -3,4 +3,18 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-image=cv2.imread('Arrow_1.jpg',cv2.THRESH_BINARY)
+image = cv2.imread('Arrow_2.jpg', cv2.THRESH_BINARY)
+print(image)
+#x,y = np.where(image == 1)
+#u_x=list(dict.fromkeys(x))
+#def width(x0,x):
+#    w = 0
+#    print(f'----{x0}----')
+#    for i in range (0,len(x)):
+#            if(x[i]==x0):
+#                w+=1
+#    return(w)
+
+#for i in range(0,len(u_x)):
+#    count = (x == u_x[i]).sum()
+#    print(f'{u_x[i]}-->{count}')
