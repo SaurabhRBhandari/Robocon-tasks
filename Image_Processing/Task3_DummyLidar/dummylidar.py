158daeaee75bf4d91199634c2bf6023ef49de340
SaurabhRBhandari <bhandarisourabhrahul@gmail.com>
2022-02-03 02:15:17 +0530

    Lidar task completed successfully

--- Image_Processing/Task3_DummyLidar/dummylidar.py
+++ Image_Processing/Task3_DummyLidar/dummylidar.py
@@ -1,8 +1,7 @@
 import PIL
-from PIL import Image,ImageDraw
+from PIL import Image, ImageDraw
 import pathlib
 import math
-import matplotlib.pyplot as plt
 
 # Here we load the world map that you need to recreate.
 image = Image.open(pathlib.Path('worldmap.jpg'))
@@ -10,10 +9,17 @@ image = image.convert('1')
 image.thumbnail((400, 400))
 image_size = min(image.size)
 
+# This stores the map created by lidar
+map = PIL.Image.new(mode="1", size=image.size)
+
+
 def get_lidar_reading(centerX, centerY, no_of_rays=360):
+    '''This mimics the lidar sensor'''
+
     lidar_reading = []
 
     if image.getpixel((centerX, centerY)) == 0:
+        # This just would'nt work in real life
         pass
     else:
         for i in range(0, 360, int(360/no_of_rays)):
@@ -28,38 +34,60 @@ def get_lidar_reading(centerX, centerY, no_of_rays=360):
                 r += 1
 
             lidar_reading.append((i, r))
+
         return lidar_reading
 
-def plot_reading_on_map(centerX,centerY,map):
-    lidar_reading = get_lidar_reading(centerX, centerY)
-    if lidar_reading:
-        print(f'{centerX},{centerY}')    
-        for i,reading in enumerate(lidar_reading):
-                try:
-                    i0=reading[0]
-                    r0=reading[1]
-                    i1=lidar_reading[i+1][0]
-                    r1=lidar_reading[i+1][1]
-                    x0 = int(r0*math.cos(i0*math.pi/180)+centerX)
-                    y0 = int((r0*math.sin(i0*math.pi/180)+centerY))
-                    
-                    x1 = int(r1*math.cos(i1*math.pi/180)+centerX)
-                    y1 = int((r1*math.sin(i1*math.pi/180)+centerY))
-                    draw=ImageDraw.Draw(map)
-                    draw.polygon([(centerX,centerY),(x0,y0),(x1,y1)], fill =255, outline =255)
-                    
-                except:
-                    continue
 
-map = PIL.Image.new(mode="1", size=(400, 400))
-plt.ion()
-plt.imshow(map)
-plt.show()
-for x in range(0,400,10):
-    for y in range(0,400,10):
-        if(map.getpixel((x,y))==0):    
-            plot_reading_on_map(x,y,map)
-            plt.imshow(map)
-            plt.show()
-            plt.pause(0.00001)
-            plt.clf()
+def plot_reading_on_map(centerX, centerY, lidar_reading, map):
+    '''This plots the collected lidar data on map'''
+    for i, reading in enumerate(lidar_reading):
+
+        try:
+            '''As the angle given by lidar is integral,
+            if we try to map all the points where lidar's light could pass,
+            we see that the generated map doesnt show the points between two light rays.
+            So here I have collected two consecutive points which lidar has detected,which are
+            expected to be at a very close distance,and then collect all the points inside the
+            triangle formed by these points-
+            1.point where the sensor is
+            2 and 3.The close consecutive points
+            Credits-MATH calculus course :)
+            '''
+            i0 = reading[0]
+            r0 = reading[1]
+
+            i1 = lidar_reading[i+1][0]
+            r1 = lidar_reading[i+1][1]
+
+            x0 = int(r0*math.cos(i0*math.pi/180)+centerX)
+            y0 = int((r0*math.sin(i0*math.pi/180)+centerY))
+
+            x1 = int(r1*math.cos(i1*math.pi/180)+centerX)
+            y1 = int((r1*math.sin(i1*math.pi/180)+centerY))
+
+            draw = ImageDraw.Draw(map)
+            draw.polygon([(centerX, centerY), (x0, y0),
+                         (x1, y1)], fill=255, outline=255)
+
+        except:
+            # to avoid encountering error at the edges
+            continue
+
+
+for x in range(0, image_size, int(image_size/20)):
+    for y in range(0, image_size, int(image_size/20)):
+
+        if(map.getpixel((x, y)) == 0):  # If a point is already seen by the lidar,it is skipped
+            
+            # Lidar won't necesarilly work for all points,it would skip the black spaces in real world
+            lidar_reading = get_lidar_reading(x, y)
+            
+            if lidar_reading:
+                
+                # These are the points where lidar will actually work
+                print(f'({x},{y})')
+                plot_reading_on_map(x, y, lidar_reading, map)
+
+
+#Finally saving the map created
+map.save('map.jpg')
--- Image_Processing/Task3_DummyLidar/trial.py
+++ Image_Processing/Task3_DummyLidar/trial.py
@@ -4,6 +4,12 @@ import pathlib
 import math
 import matplotlib.pyplot as plt
 
+
+
+'''This program is just to check if the points given by dummylidar.py program are physically viable in real world scenario'''
+
+
+
 # Here we load the world map that you need to recreate.
 image = Image.open(pathlib.Path('worldmap.jpg'))
 image = image.convert('1')
@@ -32,37 +38,56 @@ def get_lidar_reading(centerX, centerY, no_of_rays=360):
 
 def plot_reading_on_map(centerX,centerY,map):
     lidar_reading = get_lidar_reading(centerX, centerY)
-    if lidar_reading:
-        print(f'{centerX},{centerY}')    
-        for i,reading in enumerate(lidar_reading):
-                try:
-                    i0=reading[0]
-                    r0=reading[1]
-                    i1=lidar_reading[i+1][0]
-                    r1=lidar_reading[i+1][1]
-                    x0 = int(r0*math.cos(i0*math.pi/180)+centerX)
-                    y0 = int((r0*math.sin(i0*math.pi/180)+centerY))
-                    
-                    x1 = int(r1*math.cos(i1*math.pi/180)+centerX)
-                    y1 = int((r1*math.sin(i1*math.pi/180)+centerY))
-                    draw=ImageDraw.Draw(map)
-                    draw.polygon([(centerX,centerY),(x0,y0),(x1,y1)], fill =255, outline =255)
-                    
-                except:
-                    continue
+    print(f'{centerX},{centerY}')    
+    for i,reading in enumerate(lidar_reading):
+            try:
+                i0=reading[0]
+                r0=reading[1]
+                i1=lidar_reading[i+1][0]
+                r1=lidar_reading[i+1][1]
+                x0 = int(r0*math.cos(i0*math.pi/180)+centerX)
+                y0 = int((r0*math.sin(i0*math.pi/180)+centerY))
+                
+                x1 = int(r1*math.cos(i1*math.pi/180)+centerX)
+                y1 = int((r1*math.sin(i1*math.pi/180)+centerY))
+                draw=ImageDraw.Draw(map)
+                draw.polygon([(centerX,centerY),(x0,y0),(x1,y1)], fill =255, outline =255)
+                
+            except:
+                continue
 
 map = PIL.Image.new(mode="1", size=(400, 400))
 plt.ion()
-plt.axis([0,400,400,0])     
-plt.grid(False)   
-plot_reading_on_map(230,10,map)
-plot_reading_on_map(302,167,map)
-plot_reading_on_map(93,326,map)
-plot_reading_on_map(63,349,map)
+plot_reading_on_map(0,320,map)
 plt.imshow(map)
-plt.figure()
-plt.imshow(image)
 plt.show()
+plt.pause(5)
+plt.clf()
 
-plt.pause(0)
+
+plot_reading_on_map(80,140,map)
+plt.imshow(map)
+plt.show()
+plt.pause(5)
+plt.clf()
+
+
+plot_reading_on_map(200,0,map)
+plt.imshow(map)
+plt.show()
+plt.pause(5)
+plt.clf()
+
+
+plot_reading_on_map(200,280,map)
+plt.imshow(map)
+plt.show()
+plt.pause(5)
+plt.clf()
+
+
+plot_reading_on_map(280,260,map)
+plt.imshow(map)
+plt.show()
+plt.pause(5)
 plt.clf()
