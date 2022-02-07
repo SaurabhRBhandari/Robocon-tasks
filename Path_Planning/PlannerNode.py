3c62737239907a70cf4b7bf1b16e20411a0bf059
SaurabhRBhandari <bhandarisourabhrahul@gmail.com>
2022-02-08 02:29:13 +0530

    algorithm now checks if object gets closer to the map

--- Path_Planning/PlannerNode.py
+++ Path_Planning/PlannerNode.py
@@ -1,11 +1,10 @@
 import sys
 import random
 from MapNode import MapNode
-from MapClass import Map
 import numpy as np
 import tkinter
 import time
-
+import math
 
 def get_dir_backward(dir_forward):
     if(dir_forward == 'right'):
@@ -30,6 +29,7 @@ class PlannerNode:
             (self.current_obj.walls.height, self.current_obj.walls.width))
         self.blocked_places = []
         self.steps_taken = [self.current_obj.current]
+        self.dir_record = [self.current_direction]
         self.wall_callback()
 
     def wall_callback(self):
@@ -56,18 +56,22 @@ class PlannerNode:
                 next_pos = self.get_next_position(dir)
                 dir_prob[dir] -= self.visited[next_pos[0]][next_pos[1]]
                 if self.visited[next_pos[0]][next_pos[1]] == 0:
-                    dir_prob[dir] += 1
+                    dir_prob[dir] += 10
                 if next_pos == self.current_obj.walls.end:
                     dir_prob[dir] += 1000
                 if self.get_next_position(dir) in self.blocked_places:
                     dir_prob[dir] -= 1000
+                if self.closer_to_end(dir):
+                    dir_prob[dir] += 1
             v = list(dir_prob.values())
             max_prob_dir = []
             for dir in dir_prob:
                 if dir_prob[dir] == max(v):
                     max_prob_dir.append(dir)
             dir = random.choice(max_prob_dir)
-            # dir=max_prob_dir[0]
+            self.current_dir = dir
+            self.dir_record.append(dir)
+            #dir=max_prob_dir[0]
             self.current_obj.direction_callback(dir)
             if self.current_obj.current in self.steps_taken:
                 self.blocked_places.append(position)
@@ -88,12 +92,26 @@ class PlannerNode:
         elif direction == 'down':
             return (self.current_obj.current[0]+1, self.current_obj.current[1])
 
+    def closer_to_end(self, dir):
+        end = self.current_obj.walls.end
+        current = self.current_obj.current
+        dist0 = math.sqrt((current[0]-end[0])**2+(current[1]-end[1])**2)
+        next=self.get_next_position(dir)
+        dist1 = math.sqrt((next[0]-end[0])**2+(next[1]-end[1])**2)
+        return dist1<dist0
+        
+        
+        
+        
     def make_path(self):
         steps = self.steps_taken
-        # for i,step in enumerate(steps):
-        #    for j,step1 in enumerate(steps[i:]):
-        #        if step==step1:
-        #            del steps[i:j]
+        opt_steps = steps.copy()
+
+        for i, step in enumerate(steps):
+            for j, step1 in enumerate(steps[i:]):
+                if step == step1:
+                    del opt_steps[i+1:j]
+
         width = self.current_obj.walls.width
         height = self.current_obj.walls.height
         print(self.steps_taken)
@@ -101,7 +119,15 @@ class PlannerNode:
         print_root = tkinter.Tk()
         print_canvas = tkinter.Canvas(
             print_root, bg="white", height=50+height*50, width=50+width*50)
-        for step in self.steps_taken:
+        end = self.current_obj.walls.end
+        start = self.current_obj.walls.start
+        print_canvas.create_rectangle(
+            (50+(end[1]*50)), (50+(end[0]*50)), (50+((end[1]+1)*50)), (50+((end[0]+1)*50)), fill="#0000ff")
+        print_canvas.create_rectangle((50+(start[1]*50)), (50+(start[0]*50)), (50+(
+            (start[1]+1)*50)), (50+((start[0]+1)*50)), fill="#000000")
+        for step in opt_steps:
+            if step == end or step == start:
+                continue
             print_canvas.create_rectangle(
                 (50+(step[1]*50)), (50+(step[0]*50)), (50+((step[1]+1)*50)), (50+((step[0]+1)*50)), fill="#00ff00")
         print_canvas.pack()
