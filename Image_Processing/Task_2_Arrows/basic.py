71b8f648fdd2dc8b86efd7b6baace452a876a411
SaurabhRBhandari <bhandarisourabhrahul@gmail.com>
2022-02-15 23:55:53 +0530

    completed basic arrow detection

--- Image_Processing/Task_2_Arrows/basic.py
+++ Image_Processing/Task_2_Arrows/basic.py
@@ -1,3 +1,4 @@
+from re import X
 import cv2
 import numpy as np
 
@@ -27,25 +28,47 @@ def center_arrow(image):
 def get_flipped_images(image):
     image_flipped_X=np.flipud(image)
     image_flipped_Y=np.fliplr(image)
-    return image_flipped_X,image_flipped_Y
+    return image_flipped_X,image_flipped_Y  
     
 def find_orientation(match_X,match_Y,image):
     x=np.count_nonzero(match_X)
     y=np.count_nonzero(match_Y)
     print(x,y)
-    X,Y=np.where(image!=0)
+    X,Y=np.where(image==255)
     if x>y:
-        return('x')
+        for i,y in np.ndenumerate(Y):
+            if np.count_nonzero((image[:,y]))<4:
+                break
+        tip=np.array((X[i],Y[i]))-np.array(image.shape)/2
+        if tip[1]<0:
+            return('x-')
+        if tip[1]>0:
+            return('x+')
     if y>x:
-        return('y')
+        for i,x in np.ndenumerate(X):
+            if np.count_nonzero((image[x,:]))<4:
+                break
+        tip=np.array((X[i],Y[i]))-np.array(image.shape)/2
+        if tip[0]<0:
+            return('y+')
+        if tip[0]>0:
+            return('y-')
+
+#def get_tip(image):
+#    for i,x in np.ndenumerate(X):
+#        if np.count_nonzero((image[x,:]))<3:
+#            break
+#   print(X[i],Y[i])
     
-image = cv2.imread('Arrow_4.jpg', cv2.THRESH_BINARY)
-cv2.imshow('image',image)
+image = cv2.imread('Arrow_2.jpg', cv2.THRESH_BINARY)
 image = center_arrow(image)
+image[image<50]=0
+image[image>50]=255
 X_flip,Y_flip=get_flipped_images(image)
 match_X=np.where(image==255,(image==X_flip),0).astype('uint8')*255
 match_Y=np.where(image==255,(image==Y_flip),0).astype('uint8')*255
 print(find_orientation(match_X,match_Y,image))
+cv2.imshow('image',image)
 cv2.imshow('X',match_X)
 cv2.imshow('Y',match_Y)
 cv2.waitKey(0)
